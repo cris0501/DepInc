4271683f762d776e3155d24583eb4412fa67cb1e
Cris B) <cristian050198@outlook.com>
2025-05-11 14:23:49 -0600

    Fix: Add while loop to main adapter

--- adapters/ent/cli_adapter.py
+++ adapters/ent/cli_adapter.py
@@ -5,14 +5,15 @@ class CLIAdapter(InputAdapter):
         self.flight_service = flight_service
 
     def run(self):
-        print("1. Register Flight")
-        print("2. Assign Pilot")
-        option = input("Option: ")
-        if option == "1":
-            flight_id = input("Flight ID: ")
-            destination = input("Destination: ")
-            self.flight_service.register_flight(flight_id, destination)
-        elif option == "2":
-            flight_id = input("Flight ID: ")
-            pilot = input("Pilot Name: ")
-            self.flight_service.assign_pilot(flight_id, pilot)
+        while True:
+            print("1. Register Flight")
+            print("2. Assign Pilot")
+            option = input("Option: ")
+            if option == "1":
+                flight_id = input("Flight ID: ")
+                destination = input("Destination: ")
+                self.flight_service.register_flight(flight_id, destination)
+            elif option == "2":
+                flight_id = input("Flight ID: ")
+                pilot = input("Pilot Name: ")
+                self.flight_service.assign_pilot(flight_id, pilot)
